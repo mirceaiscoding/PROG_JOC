2328968c87ad86ff2d4a21d4a2aa4d3f94d6f22d
Troaca Denis <denistroaca@yahoo.com>
2022-10-20 01:10:07 +0300

    Added aiming and shooting
    The player can aim and shoot bullets by using the mouse

---
+++ 2d-shooter/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+
+    // RigidBody of bullet
+    public Rigidbody2D rigidbody;
+
+}
---
+++ 2d-shooter/Assets/Scripts/GunFire.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour
+{
+    // A bullet that can be fired from the gun. Set in Unity Editor
+    public GameObject bullet;
+
+    // The spawning point of the bullet. Set in Unity Editor
+    public Transform firePoint; 
+
+    // The speed of the bullet. Set in Unity Editor
+    public float fireForce;
+
+    //Spawns a bullet then makes it move to the right of the spawn point (where the mouse is pointed)
+    public void Fire()
+    {
+        GameObject projectile = Instantiate(bullet, firePoint.position, firePoint.rotation);
+        projectile.GetComponent<Rigidbody2D>().AddForce(firePoint.right * fireForce, ForceMode2D.Impulse);
+    }
+}
+
