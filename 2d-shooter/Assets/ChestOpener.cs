7f8c1c9ff7febed5116535f3383618725c08d46c
Robertto <73616883+crime-story@users.noreply.github.com>
2022-11-06 21:34:32 +0200

    feat(#27): added range for chests
    Co-Authored-By: linterobert <80642370+linterobert@users.noreply.github.com>

--- 2d-shooter/Assets/ChestOpener.cs
+++ 2d-shooter/Assets/ChestOpener.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class ChestOpener : MonoBehaviour
 {
+    /*
+    public bool isOpen;
+    public Animator animator;
+
+    public void OpenChest()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            Debug.Log("Chest now open...");
+            animator.SetBool("IsOpen", isOpen);
+        }
+    }
+    */
+
+    /
     public GameObject ChestClose, ChestOpen;
 
 
@@ -18,9 +34,41 @@ public class ChestOpener : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetKeyDown("E"))
+        {
+            if (ChestClose == true)
+            {
+                ChestClose.SetActive(false);
+                ChestOpen.SetActive(true);
+            }
+            else
+            {
+                ChestClose.SetActive(true);
+                ChestOpen.SetActive(false);
+            }
+        }
+        /*
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;
 
+        // Update variable for animator to switch between idle and running.
+        animator.SetFloat("Speed", Mathf.Abs(moveX) + Mathf.Abs(moveY));
+
+        // Flip player based on horizontal movement
+        if (moveX > 0)
+        {
+            // normal
+            spriteRenderer.flipX = false;
+        }
+        else if (moveX < 0)
+        {
+            // moving to the left -> flip
+            spriteRenderer.flipX = true;
+        }
+        */
+    }
+    /*
     void OnTriggerEnter2D(Collider2D collision)
     {
         ChestClose.SetActive(false);
@@ -32,4 +80,6 @@ public class ChestOpener : MonoBehaviour
         ChestClose.SetActive(true);
         ChestOpen.SetActive(false);
     }
+    */
+    
 }
