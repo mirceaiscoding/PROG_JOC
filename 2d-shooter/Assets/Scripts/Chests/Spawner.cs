301abe6b60bffc51b505262d7005f7d0d446aa14
crime-story <73616883+crime-story@users.noreply.github.com>
2022-12-22 13:02:49 +0200

    feat(#13): wip
    Co-Authored-By: linterobert <80642370+linterobert@users.noreply.github.com>

--- 2d-shooter/Assets/Scripts/Chests/Spawner.cs
+++ 2d-shooter/Assets/Scripts/Chests/Spawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject[] items; // list of prefabs for the items that you want to spawn
+
     public GameObject objectToSpawn;
 
     public GameObject spawnToObject;
@@ -18,5 +20,22 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // check if the chest has been opened
+        if (!GetComponent<Animation>().isPlaying)
+        {
+            // play the chest opening animation
+            GetComponent<Animation>().Play();
+
+            // choose a random item from the list
+            int index = Random.Range(0, items.Length);
+            GameObject item = items[index];
+
+            // instantiate the item at the chest's position
+            Instantiate(item, transform.position, Quaternion.identity);
+        }
     }
 }
