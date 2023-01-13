5d21e8de4f80241f30fced7173047923f6087bb8
Bina Mircea <bina.mircea2001@gmail.com>
2023-01-13 15:55:07 +0200

    Fixed multiple audio listeners bug

--- 2d-shooter/Assets/DontDestroyAudio.cs
+++ 2d-shooter/Assets/DontDestroyAudio.cs
@@ -8,17 +8,27 @@ public class DontDestroyAudio : MonoBehaviour
     private static DontDestroyAudio instance = null;
     public AudioSource audioSource;
 
+    private void disablePlayerAudioListener() {
+        GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
+        player.transform.Find("Main Camera").GetComponent<AudioListener>().enabled = false;
+    }
+
     private void Awake()
     {
+        AudioListener[] aL = FindObjectsOfType<AudioListener>();
+        if (aL.Length > 1) {
+            disablePlayerAudioListener();
+        }
+
         if (instance != null && instance != this)
         {
+            Debug.Log("There is already an audio source. Destroy the new one.");
             Destroy(this.gameObject);
             return;
         }
-        else
-        {
-            instance = this;
-        }
+
+        Debug.Log("An instance of audio source was created.");
+        instance = this;
         DontDestroyOnLoad(this.gameObject);
     }
 
