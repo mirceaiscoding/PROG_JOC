98ede9c9d7e6bb5e9840e3abe286bab0e9a5ec49
Bina Mircea <bina.mircea2001@gmail.com>
2023-01-14 18:25:23 +0200

    Stop walking sound when player is no longer moving.

--- 2d-shooter/Assets/Scripts/Player/PlayerMovement.cs
+++ 2d-shooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -108,6 +108,8 @@ public class PlayerMovement : MonoBehaviour
             {
                 walkSoundEffect.Play();
             }
+        } else {
+            walkSoundEffect.Stop();
         }
     }
 
